865d57a202da6f89e341ccb242c8572567256d22
rchaser53 <tayoshizawa29@gmail.com>
2019-03-20 00:17:12 +0900

    try to use OneToOne

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {  PrimaryGeneratedColumn, createConnection, OneToMany, JoinColumn,
+import {  PrimaryGeneratedColumn, createConnection, OneToMany, JoinColumn, OneToOne,
           getConnection, Entity, ManyToOne, ConnectionOptions, PrimaryColumn, Column} from "typeorm";
 
 @Entity('test_table')
@@ -25,6 +25,26 @@ export class Photo {
   @ManyToOne(type => TestTable, test_table => test_table.photos)
   @JoinColumn({ name: 'test_table_id' })
   test_table_id?: TestTable;
+
+  @OneToOne(type => PhotoInfo, photoInfo => photoInfo.photo)
+  @JoinColumn({ name: 'photo_id' })
+  photo_info: PhotoInfo;
+}
+
+@Entity('photo_info')
+export class PhotoInfo {
+  @PrimaryColumn("varchar")
+  id: string;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @Column({ nullable: false })
+  photo_id: string;
+
+  @OneToOne(type => Photo, photo => photo.photo_info)
+  @JoinColumn({ name: 'photo_id' })
+  photo: Photo;
 }
 
 const createPhoto = (id, url, test_table_id) => {
@@ -43,24 +63,34 @@ const options: ConnectionOptions = {
   "username": "root",
   "password": "root",
   "database": "test_database",
-  entities: [TestTable, Photo]
+  entities: [TestTable, Photo, PhotoInfo],
+  logging: ["query", "error"]
 };
+
+const createPhotoInfo = (id, description, photo_id): PhotoInfo => {
+  const photoInfo = new PhotoInfo()
+  photoInfo.id = id
+  photoInfo.description = description
+  photoInfo.photo_id = photo_id
+  return photoInfo
+}
 
 (async () => {
   try {
-    const id ='id_d'
+    const id ='photo_id_a'
     const connection = await createConnection(options);
     await connection.transaction(async (transactionalEntityManager) => {
-      const photoA = createPhoto('a', 'url-a', id)
-      let testTable = new TestTable();
-      testTable.id = id;
-      testTable.num_field = 3001;
-      testTable.photos = [
-        createPhoto('a', 'url-a', id),
-      ];
+      // const photoA = createPhoto('b', 'url-b', id)
+      // let testTable = new TestTable();
+      // testTable.id = id;
+      // testTable.num_field = 3001;
+      // testTable.photos = [
+      //   createPhoto('a', 'url-a', id),
+      // ];
+      
 
-      await transactionalEntityManager.save(testTable)
-      await transactionalEntityManager.save(photoA)
+      // await transactionalEntityManager.save(testTable)
+      await transactionalEntityManager.save(createPhotoInfo('photoId', 'test_desc', id))
     })
 
     // await postRepository.update(testTable, {
