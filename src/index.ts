6ad48cb9d5c30d4fa022f0778feaec17d2293d45
rchaser53 <tayoshizawa29@gmail.com>
2019-03-28 00:13:32 +0900

    learn ManyToOne and OneToMany

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ export class TestTable {
     @Column({ nullable: false })
     num_field: number;
 
-    @OneToMany(type => Photo, photo => photo.test_table_id)
+    @OneToMany(type => Photo, photo => photo.test_table)
+    @JoinColumn({ name: 'id' })
     photos: Photo[];
 }
 
@@ -24,10 +25,13 @@ export class Photo {
 
   @ManyToOne(type => TestTable, test_table => test_table.photos)
   @JoinColumn({ name: 'test_table_id' })
-  test_table_id?: TestTable;
+  test_table: TestTable
+
+  @Column()
+  test_table_id?: string;
 
   @OneToOne(type => PhotoInfo, photoInfo => photoInfo.photo)
-  @JoinColumn({ name: 'photo_id' })
+  @JoinColumn({ name: 'id' })
   photo_info: PhotoInfo;
 }
 
@@ -79,7 +83,7 @@ const createPhotoInfo = (id, description, photo_id): PhotoInfo => {
   try {
     const id ='photo_id_a'
     const connection = await createConnection(options);
-    await connection.transaction(async (transactionalEntityManager) => {
+    await connection.transaction(async (manager) => {
       // const photoA = createPhoto('b', 'url-b', id)
       // let testTable = new TestTable();
       // testTable.id = id;
@@ -88,9 +92,22 @@ const createPhotoInfo = (id, description, photo_id): PhotoInfo => {
       //   createPhoto('a', 'url-a', id),
       // ];
       
+      const photos = await manager
+            .getRepository(Photo)
+            .createQueryBuilder("photo")
+            .innerJoinAndSelect("photo.test_table", "test_table")
+            .getMany();
+      console.log(photos);
+
+      const test_table = await manager
+            .getRepository(TestTable)
+            .createQueryBuilder("test_table")
+            .innerJoinAndSelect("test_table.photos", "photos")
+            .getMany();
+      console.log(test_table)
 
       // await transactionalEntityManager.save(testTable)
-      await transactionalEntityManager.save(createPhotoInfo('photoId', 'test_desc', id))
+      // await transactionalEntityManager.save(createPhotoInfo('photoId', 'test_desc', id))
     })
 
     // await postRepository.update(testTable, {
@@ -111,4 +128,9 @@ const createPhotoInfo = (id, description, photo_id): PhotoInfo => {
   } catch (error) {
     throw new Error(error)
   }
-})()
+  return 'finish'
+})().then((ret) => {
+  console.log(ret)
+}).catch((err) => {
+  console.error(err)
+})
